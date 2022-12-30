7b4af18be0ece65652e77591a7d9300ed0ac7b74
chorimgenius <artyum15@kakao.com>
2022-12-30 15:29:03 +0900

    refactor: articles PEP 수정(Black) #37
    refactor: articles PEP 수정(Black)
    
    Close #이슈번호

--- article/models.py
+++ article/models.py
@@ -1,31 +1,39 @@
 from django.db import models
 from user.models import User
 
+
 class Draft(models.Model):
     style = models.CharField(max_length=50)
     image = models.ImageField(null=True, blank=True)
-    
+
     def __str__(self):
         return self.style
-    
+
+
 class Style(models.Model):
-    image = models.ImageField(upload_to='style/')
-    
+    image = models.ImageField(upload_to="style/")
+
     def __str__(self):
         return self.image
 
+
 class Size(models.Model):
-    draft = models.ForeignKey(Draft,on_delete=models.CASCADE, related_name='draft_set')
+    draft = models.ForeignKey(Draft, on_delete=models.CASCADE, related_name="draft_set")
     size = models.CharField(max_length=50)
     price = models.IntegerField()
-    
+
     def __str__(self):
-        return (f"{self.draft}, {self.size}")
+        return f"{self.draft}, {self.size}"
+
 
 class Article(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
-    draft = models.ForeignKey(Draft,on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    draft = models.ForeignKey(Draft, on_delete=models.CASCADE, null=True)
     style = models.ForeignKey(Style, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='result/', max_length=255, null=True,)
+    image = models.ImageField(
+        upload_to="result/",
+        max_length=255,
+        null=True,
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     likes = models.ManyToManyField(User, related_name="user_likes")
--- article/urls.py
+++ article/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from article import views
 
 urlpatterns = [
-    path('article/', views.CustomView.as_view()),
-    path('', views.RankArticleView.as_view(), name='order_view'),
-    path('<int:article_id>/like/', views.LikeView.as_view(), name='ClothesLike'),
+    path("article/", views.CustomView.as_view()),
+    path("", views.RankArticleView.as_view(), name="order_view"),
+    path("<int:article_id>/like/", views.LikeView.as_view(), name="ClothesLike"),
 ]
