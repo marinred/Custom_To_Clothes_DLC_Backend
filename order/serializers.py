b717f7850b4454b89167c1ad3e48c09551ea806e
jinyeobjeong <yeobdol@gmail.com>
2022-12-30 15:17:08 +0900

    Fix: order리팩토링 #34
    - order 리팩토링
    
    Close #34

--- order/serializers.py
+++ order/serializers.py
@@ -6,64 +6,96 @@ from article.models import Article
 class OrderViewSerializer(serializers.ModelSerializer):
     article_user = serializers.SerializerMethodField()
 
-    def get_article_user(self, obj):    
+    def get_article_user(self, obj):
         return obj.user.username
-    
+
     class Meta:
         model = Article
-        fields = ('id', 'article_user', 'image', 'user', 'draft',)
+        fields = (
+            "id",
+            "article_user",
+            "image",
+            "user",
+            "draft",
+        )
+
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
-        fields = ('mount', 'status',)
-        
+        fields = (
+            "mount",
+            "status",
+        )
+
+
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields = ('image',) 
-        
+        fields = ("image",)
+
+
 class CartViewSerializer(serializers.ModelSerializer):
     article_user = serializers.SerializerMethodField()
     price = serializers.SerializerMethodField()
     size = serializers.SerializerMethodField()
     article = ArticleSerializer()
-    
-    def get_article_user(self, obj):    
+
+    def get_article_user(self, obj):
         return obj.user.username
-    
+
     def get_price(self, obj):
         return obj.size.price
-    
+
     def get_size(self, obj):
         return obj.size.size
-    
+
     class Meta:
         model = Order
-        fields = ('article_user','id','mount','size','article','user','price',)
+        fields = (
+            "article_user",
+            "id",
+            "mount",
+            "size",
+            "article",
+            "user",
+            "price",
+        )
+
 
 class OrderListViewSerializer(serializers.ModelSerializer):
     article_user = serializers.SerializerMethodField()
     price = serializers.SerializerMethodField()
     size = serializers.SerializerMethodField()
     article = ArticleSerializer()
-    
-    def get_article_user(self, obj):    
+
+    def get_article_user(self, obj):
         return obj.user.username
-    
+
     def get_price(self, obj):
         return obj.size.price
-    
+
     def get_size(self, obj):
         return obj.size.size
-    
+
     class Meta:
         model = Order
-        fields = ('article_user','id','mount','size','status','article','user','price',)
+        fields = (
+            "article_user",
+            "id",
+            "mount",
+            "size",
+            "status",
+            "article",
+            "user",
+            "price",
+        )
 
 
 class OrderCreateSerializer(serializers.ModelSerializer):
-      
     class Meta:
         model = Order
-        fields = ('status','id',)
+        fields = (
+            "status",
+            "id",
+        )
--- order/views.py
+++ order/views.py
@@ -1,55 +1,59 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from order.serializers import OrderSerializer, OrderViewSerializer, CartViewSerializer, OrderListViewSerializer, OrderCreateSerializer
+from order.serializers import (
+    OrderSerializer,
+    OrderViewSerializer,
+    CartViewSerializer,
+    OrderListViewSerializer,
+    OrderCreateSerializer,
+)
 
 from .models import Article
 from .models import Size
 from .models import Order
 
-#clothes_profile
+# clothes_profile
 class OrderView(APIView):
     def get(self, request, article_id):
-         article = Article.objects.get(id=article_id)
-         article_serializer = OrderViewSerializer(article)
-         return Response(article_serializer.data, status=status.HTTP_200_OK)
-     
-    def post(self, request, article_id): 
+        article = Article.objects.get(id=article_id)
+        article_serializer = OrderViewSerializer(article)
+        return Response(article_serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, article_id):
         serializer = OrderSerializer(data=request.data)
         if serializer.is_valid():
-            article = Article.objects.get(id=article_id) 
-            size = Size.objects.get(draft_id=article.draft_id, size=request.data["size"]) 
-            serializer.save(article_id=article_id, size=size, user=request.user) 
+            article = Article.objects.get(id=article_id)
+            size = Size.objects.get(
+                draft_id=article.draft_id, size=request.data["size"]
+            )
+            serializer.save(article_id=article_id, size=size, user=request.user)
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-#cart (image, size, mount, price)   
+# cart (image, size, mount, price)
 class CartView(APIView):
-    def get(self, request): 
+    def get(self, request):
         order = Order.objects.filter(user=request.user, status=0)
         cart_serializer = CartViewSerializer(order, many=True)
         return Response(cart_serializer.data, status=status.HTTP_200_OK)
-    
-    
+
     def put(self, request):
         order = Order.objects.filter(user=request.user, status=0)
         for i in range(len(order)):
-            serializer = OrderCreateSerializer(order[i], data={'status':1})
+            serializer = OrderCreateSerializer(order[i], data={"status": 1})
             if serializer.is_valid():
                 serializer.save()
-            else: 
+            else:
                 return Response("수정실패!!!", status=status.HTTP_400_BAD_REQUEST)
-        return Response("수정완료",status=status.HTTP_200_OK)
+        return Response("수정완료", status=status.HTTP_200_OK)
 
-           
 
-#orderlist(size, mount, price, status)
+# orderlist(size, mount, price, status)
 class OrderListView(APIView):
-    def get(self, request): 
+    def get(self, request):
         order = Order.objects.filter(user=request.user, status=1)
         orderlist_serializer = OrderListViewSerializer(order, many=True)
         return Response(orderlist_serializer.data, status=status.HTTP_200_OK)
-
-        
