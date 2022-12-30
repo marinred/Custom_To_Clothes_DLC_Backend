da445d8c7f88cce1035529576fe5358d72e91eb2
marinred <endrms1995@gmail.com>
2022-12-30 15:23:34 +0900

    Fix: 프로젝트 black style 적용 #33
    - 프로젝트 pep 수정 (black code style)
    - 작업내용 2
    
    Close #33

--- order/models.py
+++ order/models.py
@@ -3,9 +3,10 @@ from article.models import Size
 from article.models import Article
 from user.models import User
 
+
 class Order(models.Model):
-    size = models.ForeignKey(Size,on_delete=models.CASCADE)
-    article = models.ForeignKey(Article,on_delete=models.CASCADE)
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    size = models.ForeignKey(Size, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     mount = models.IntegerField()
     status = models.IntegerField(default=0)
